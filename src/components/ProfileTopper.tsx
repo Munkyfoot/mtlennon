f0b0aa00d5f3eef5997c5a9ee3e96b6a35d6052d
Munkyfoot <wopas182@gmail.com>
2023-11-14 13:33:08 -0800

    Create ProfileTopper component with portrait, name and links to email and relevant profiles.

---
+++ src/components/ProfileTopper.tsx
@@ -0,0 +1,185 @@
+import { CopyIcon, EmailIcon, CheckCircleIcon } from "@chakra-ui/icons"
+import {
+    Button,
+    Circle,
+    Flex,
+    HStack,
+    Heading,
+    Icon,
+    IconButton,
+    Text,
+    VStack,
+    useToast,
+} from "@chakra-ui/react"
+import NextImage from "next/image"
+import { FaGithub, FaLinkedin } from "react-icons/fa"
+import portrait from "/public/mp.png"
+import { useState } from "react"
+
+const ProfileTopper = () => {
+    const [emailIsCopied, setEmailIsCopied] = useState(false)
+
+    const toast = useToast()
+
+    return (
+        <Flex
+            direction={{
+                base: "column",
+                md: "row",
+            }}
+            align="center"
+            justify="center"
+            gap={8}
+        >
+            <Circle size={64} maxW="full" overflow="hidden" shadow="lg">
+                <NextImage
+                    src={portrait}
+                    alt="Michael Lennon"
+                    placeholder="blur"
+                />
+            </Circle>
+            <VStack
+                spacing={4}
+                align={{
+                    base: "center",
+                    md: "flex-start",
+                }}
+            >
+                <VStack
+                    align={{
+                        base: "center",
+                        md: "flex-start",
+                    }}
+                    textAlign={{
+                        base: "center",
+                        md: "left",
+                    }}
+                >
+                    <Heading as="h1" size="2xl">
+                        Michael Lennon
+                    </Heading>
+                    <Text fontSize="2xl">Software Developer & Designer</Text>
+                </VStack>
+
+                <VStack
+                    w="full"
+                    align={{
+                        base: "center",
+                        md: "flex-start",
+                    }}
+                    spacing={3}
+                >
+                    <HStack w="full">
+                        <Button
+                            as="a"
+                            href="mailto:michael&#64;michaeltlennon&#46;com"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            leftIcon={<EmailIcon />}
+                            variant="outline"
+                            size={{
+                                base: "md",
+                                md: "sm",
+                            }}
+                            w={{
+                                base: "full",
+                                md: "auto",
+                            }}
+                        >
+                            michael&#64;michaeltlennon&#46;com
+                        </Button>
+                        <IconButton
+                            aria-label="Copy Email"
+                            icon={
+                                emailIsCopied ? (
+                                    <CheckCircleIcon />
+                                ) : (
+                                    <CopyIcon />
+                                )
+                            }
+                            onClick={() => {
+                                navigator.clipboard
+                                    .writeText("michael@michaeltlennon.com")
+                                    .then(() => {
+                                        setEmailIsCopied(true)
+                                        setTimeout(() => {
+                                            setEmailIsCopied(false)
+                                        }, 3000)
+
+                                        toast({
+                                            title: "Email Copied",
+                                            description:
+                                                "Email address copied to clipboard.",
+                                            status: "success",
+                                            duration: 3000,
+                                            isClosable: true,
+                                        })
+                                    })
+                            }}
+                            colorScheme={emailIsCopied ? "green" : "gray"}
+                            isDisabled={emailIsCopied}
+                            size={{
+                                base: "md",
+                                md: "sm",
+                            }}
+                            variant="ghost"
+                        />
+                    </HStack>
+                    <Flex
+                        w="full"
+                        direction={{
+                            base: "column",
+                            md: "row",
+                        }}
+                        align={{
+                            base: "center",
+                            md: "flex-start",
+                        }}
+                        gap={3}
+                    >
+                        <Button
+                            as="a"
+                            href="https://github.com/Munkyfoot"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            leftIcon={<Icon as={FaGithub} />}
+                            colorScheme="purple"
+                            variant="outline"
+                            size={{
+                                base: "md",
+                                md: "sm",
+                            }}
+                            w={{
+                                base: "full",
+                                md: "auto",
+                            }}
+                        >
+                            GitHub Profile
+                        </Button>
+                        <Button
+                            as="a"
+                            href="https://www.linkedin.com/in/munkyfoot/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            leftIcon={<Icon as={FaLinkedin} />}
+                            colorScheme="blue"
+                            variant="outline"
+                            size={{
+                                base: "md",
+                                md: "sm",
+                            }}
+                            w={{
+                                base: "full",
+                                md: "auto",
+                            }}
+                        >
+                            LinkedIn Profile
+                        </Button>
+                    </Flex>
+                </VStack>
+            </VStack>
+        </Flex>
+    )
+}
+
+export default ProfileTopper
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,5 @@
 import Layout from "@/components/Layout"
-import {
-    Circle,
-    Code,
-    Flex,
-    Heading,
-    List,
-    ListItem,
-    Text,
-    UnorderedList,
-    VStack,
-} from "@chakra-ui/react"
-import NextImage from "next/image"
-import portrait from "/public/mp.png"
+import ProfileTopper from "@/components/ProfileTopper"
 
 export default function Home() {
     return (
@@ -19,41 +7,7 @@ export default function Home() {
             title="Michael Lennon"
             description="Michael Lennon is a software developer, designer, and musician."
         >
-            <Flex
-                direction={{
-                    base: "column",
-                    md: "row",
-                }}
-                align="center"
-                justify="center"
-                gap={8}
-            >
-                <Circle size={64} maxW="full" overflow="hidden" shadow="lg">
-                    <NextImage
-                        src={portrait}
-                        alt="Michael Lennon"
-                        placeholder="blur"
-                    />
-                </Circle>
-                <VStack
-                    spacing={2}
-                    align={{
-                        base: "center",
-                        md: "flex-start",
-                    }}
-                    textAlign={{
-                        base: "center",
-                        md: "left",
-                    }}
-                >
-                    <Heading as="h1" size="2xl">
-                        Michael Lennon
-                    </Heading>
-                    <Text fontSize="2xl">
-                        Software Developer · Designer · Musician
-                    </Text>
-                </VStack>
-            </Flex>
+            <ProfileTopper />
         </Layout>
     )
 }

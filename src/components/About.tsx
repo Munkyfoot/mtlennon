992a9a8f9ba07dd95c194264b20c7d90ff95bab8
Munkyfoot <wopas182@gmail.com>
2023-11-15 12:40:00 -0800

    Update About content again.

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -11,66 +11,63 @@ const About = () => {
                 About
             </Heading>
             <Text>
-                As a dedicated and creative Front-End Developer, my journey in
-                the tech world has been anything but conventional. My
-                professional career began in the music industry as a touring
-                musician, where I honed not only my artistic skills but also my
-                abilities in communication, adaptability, and self-management.
-                Parallel to my music career, I nurtured a longstanding passion
-                for software development, a journey that started in my childhood
-                with simple HTML and CSS to spruce up my MySpace page. This
-                passion grew alongside my musical endeavors, leading me to build
-                websites for friends, family, and various projects throughout
-                high school and beyond.
+                As a seasoned Software Developer and Designer, I bring over five
+                years of professional experience and a lifelong passion for
+                technology. My journey has been marked by a unique blend of
+                artistic creativity and technical prowess, beginning with simple
+                adjustments of HTML and CSS and evolving into a career as a
+                freelance developer building, launching and maintaining over a
+                dozen bespoke websites. I hold a Bachelor's Degree in Computer
+                Science from Western Governors University, and my diverse skill
+                set spans front-end and back-end web development, game
+                development, and machine learning.
             </Text>
             <div>
                 <Collapse in={isOpen} animateOpacity>
                     <VStack align="flex-start">
                         <Text>
-                            In the ever-evolving landscape of technology, I have
-                            continually expanded my skill set, diving into web
-                            development, game development, and machine learning.
-                            I've become proficient in a range of programming
-                            languages, including Python, TypeScript, and C#, and
-                            have developed a deep understanding of both
-                            front-end and back-end development. My experience is
-                            further bolstered by my completion of several
-                            Udacity Nanodegrees, which have refined my skills in
-                            Full Stack Web Development, Artificial Intelligence,
-                            and C++.
+                            My career began in the vibrant world of music as a
+                            touring musician, where I honed key soft skills such
+                            as communication, adaptability, and collaborative
+                            problem-solving. This background in the arts,
+                            coupled with a deep interest in software
+                            development, enables me to approach problem-solving
+                            and design in innovative ways. As I transitioned
+                            from music to a full-time career in software
+                            development, I established my own web development
+                            business, further deepening my understanding of
+                            business operations and enhancing my client
+                            relations skills.
                         </Text>
                         <Text>
-                            The decision to pivot my career from music to
-                            software development was driven by a desire for new
-                            challenges and a longing to plant roots and start a
-                            family. Founding my own web development business was
-                            a natural progression, allowing me to leverage my
-                            diverse skill set and client management experience.
-                            This venture not only expanded my technical
-                            expertise but also deepened my understanding of
-                            business operations and client relations.
+                            I am proficient in a range of technologies, with a
+                            current focus on React/Next.js and ChakraUI hosted
+                            on Vercel, reflecting my commitment to performance,
+                            aesthetic appeal, and accessibility in web
+                            development. I also have experience with Flask and
+                            Bootstrap, alongside cloud management, and a working
+                            knowledge of languages like Python, TypeScript, C#,
+                            C++, and Java. My technical toolkit includes git for
+                            version control, Lighthouse for performance
+                            analysis, and the OpenAI API for experimentation.
                         </Text>
                         <Text>
-                            Now, as a recent graduate with a Bachelor's Degree
-                            in Computer Science from Western Governors
-                            University, I am eager to apply my unique blend of
-                            skills and experiences in a collaborative team
-                            environment. I am particularly excited about
-                            opportunities where I can contribute to innovative
-                            front-end development projects and continue growing
-                            as a software developer. My diverse background,
-                            encompassing both artistic and technical realms, has
-                            equipped me with a unique perspective that I believe
-                            will add significant value to any team.
+                            My educational journey includes Udacity Nanodegrees
+                            in Full Stack Web Development, Artificial
+                            Intelligence, and C++, which not only enhanced my
+                            technical skills but also enabled me to mentor and
+                            teach others, as demonstrated during my tenure as a
+                            Program Mentor at Udacity.
                         </Text>
                         <Text>
-                            My journey has been one of continuous learning and
-                            adaptation. I bring a rich tapestry of experiences,
-                            technical skills, and a passion for creative
-                            problem-solving to the table. I am excited about the
-                            prospect of joining a team where I can contribute,
-                            grow, and explore the vast possibilities of software
-                            development.
+                            I am excited about opportunities to contribute to
+                            innovative projects in front-end development, where
+                            I can blend my unique artistic vision with technical
+                            expertise. As a recent graduate, I am eager to join
+                            a dynamic team where I can apply my creative
+                            problem-solving abilities and technical skills,
+                            contribute meaningfully, and continue to grow in the
+                            ever-evolving field of software development.
                         </Text>
                     </VStack>
                 </Collapse>

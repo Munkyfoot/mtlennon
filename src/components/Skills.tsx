5a57a716fafb8e8521d3b774c1d93856359bfed1
Munkyfoot <wopas182@gmail.com>
2023-11-16 12:32:58 -0800

    Update Skills section.

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -10,29 +10,51 @@ const Skills = () => {
                 <Heading as="h3" size="md">
                     Languages
                 </Heading>
-                <Text>Python, TypeScript, JavaScript, HTML, CSS, C#, C++</Text>
+                <VStack align="flex-start" spacing={0}>
+                    <Heading as="h4" size="sm">
+                        Adept
+                    </Heading>
+                    <Text>Python, TypeScript, JavaScript, HTML, CSS</Text>
+                </VStack>
+                <VStack align="flex-start" spacing={0}>
+                    <Heading as="h4" size="sm">
+                        Proficient
+                    </Heading>
+                    <Text>Bash/Shell, CMD, C#, C++</Text>
+                </VStack>
             </VStack>
             <VStack align="flex-start">
                 <Heading as="h3" size="md">
-                    Frameworks/Software
+                    Frameworks/Software/APIs
                 </Heading>
-                <Text>
-                    Next.js/React, ChakraUI, Tailwind, Flask, Bootstrap,
-                    Supabase, SQLAlchemy, Unity Game Engine
-                </Text>
+                <VStack align="flex-start" spacing={0}>
+                    <Heading as="h4" size="sm">
+                        Adept
+                    </Heading>
+                    <Text>
+                        Next.js/React, ChakraUI, Flask, Bootstrap, SQLAlchemy,
+                        Unity Game Engine, OpenAI API, StableDiffusion
+                    </Text>
+                </VStack>
+                <VStack align="flex-start" spacing={0}>
+                    <Heading as="h4" size="sm">
+                        Proficient
+                    </Heading>
+                    <Text>Tailwind, Supabase, Apache 2, PyTorch</Text>
+                </VStack>
             </VStack>
             <VStack align="flex-start">
                 <Heading as="h3" size="md">
                     Tools
                 </Heading>
-                <Text>Git, VS Code, Anaconda</Text>
+                <Text>Git, VS Code, Anaconda, CLI (Shell & CMD)</Text>
             </VStack>
             <VStack align="flex-start">
                 <Heading as="h3" size="md">
                     Development Focuses
                 </Heading>
                 <Text>
-                    Front-End Web Development, Game Development, Back-End
+                    Front-End Web Development, Back-End Development, Game
                     Development, Machine Learning
                 </Text>
             </VStack>

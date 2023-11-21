b1d39a2aebe658a521243b1a19763d3b30608ad2
Munkyfoot <wopas182@gmail.com>
2023-11-21 14:39:32 -0800

    Update about. Show all from start and remove expandable section.

--- src/components/tabs/About.tsx
+++ src/components/tabs/About.tsx
@@ -9,92 +9,38 @@ import {
 } from "@chakra-ui/react"
 
 const About = () => {
-    const { isOpen, onToggle } = useDisclosure()
-
     return (
         <VStack align="flex-start">
             <Heading as="h2" size="lg">
                 Career
             </Heading>
             <Text>
-                As a Software Developer and Designer, I bring over five years of
-                professional experience and a lifelong passion for technology.
-                My journey has been marked by a unique blend of artistic
-                creativity and technical interest, beginning with simple
-                adjustments of HTML and CSS and evolving into a career as a
-                freelance developer. I&apos;ve had the opportunity to work with
-                a variety of clients to build, launch, and maintain over a dozen
-                professional websites. I hold a Bachelor&apos;s Degree in
-                Computer Science from Western Governors University, and my
-                diverse skillset spans front-end and back-end web development,
-                game development, and machine learning.
+                As a Software Developer and Designer with over five years of
+                experience, I have evolved from tinkering with HTML and CSS to
+                becoming a proficient freelance developer. My career journey is
+                marked by a blend of artistic creativity and technical interest,
+                culminating in the creation and maintenance of over a dozen
+                professional websites. Holding a Bachelor's Degree in Computer
+                Science from Western Governors University, I have developed a
+                diverse skill set covering front-end and back-end web
+                development, game development, and machine learning. My
+                technical proficiency spans React/Next.js, ChakraUI, Flask,
+                Bootstrap, and a range of languages including Python,
+                TypeScript, C#, C++, and Java, coupled with tools like Git.
             </Text>
-            <div>
-                <Collapse in={isOpen} animateOpacity>
-                    <VStack align="flex-start">
-                        <Text>
-                            My career began in the vibrant world of music as a
-                            touring musician, where I honed key soft skills such
-                            as communication, adaptability, and collaborative
-                            problem-solving. This background in the arts,
-                            coupled with a deep interest in software
-                            development, enables me to approach problem-solving
-                            and design in innovative ways. As I continued to
-                            develop my career in software development, I
-                            established my own web development business, further
-                            deepening my understanding of business operations
-                            and enhancing my client relations skills.
-                        </Text>
-                        <Text>
-                            I am proficient in a range of technologies, with a
-                            current focus on React/Next.js and ChakraUI hosted
-                            on Vercel, reflecting my commitment to performance,
-                            aesthetic appeal, and accessibility in web
-                            development. This is complemented by experience with
-                            Flask and Bootstrap, alongside server management,
-                            and a working knowledge of languages like Python,
-                            TypeScript, C#, C++, and Java. My technical toolkit
-                            includes Git for version control, Unity for Game
-                            Development, the OpenAI API for experimentation, as
-                            well as many other technologies.
-                        </Text>
-                        <Text>
-                            In addition to my Computer Science degree, my
-                            educational experience includes Udacity Nanodegrees
-                            in Full Stack Web Development, Artificial
-                            Intelligence, and C++. These courses not only
-                            enhanced my technical skills but also enabled me to
-                            mentor and teach others, as demonstrated during my
-                            time as a Program Mentor at Udacity.
-                        </Text>
-                        <Text>
-                            I look forward to opportunities to contribute to
-                            innovative projects in front-end development, where
-                            I can blend my unique artistic vision with technical
-                            expertise. As a recent graduate, I am eager to join
-                            a dynamic team where I can apply my creative
-                            problem-solving abilities and technical skills,
-                            contribute meaningfully, and continue to grow in the
-                            ever-evolving field of software development.
-                        </Text>
-                    </VStack>
-                </Collapse>
-                <Button
-                    variant="link"
-                    colorScheme="blue"
-                    onClick={onToggle}
-                    leftIcon={
-                        isOpen ? (
-                            <MinusIcon boxSize={2} />
-                        ) : (
-                            <AddIcon boxSize={2} />
-                        )
-                    }
-                    size="sm"
-                >
-                    {isOpen ? "Show Less" : "Read More"}
-                </Button>
-            </div>
+            <Text>
+                My background as a touring musician has imbued me with key soft
+                skills such as communication, adaptability, and collaborative
+                problem-solving. This experience, alongside my educational
+                pursuits including Udacity Nanodegrees in Full Stack Web
+                Development, Artificial Intelligence, and C++, has shaped my
+                ability to approach challenges innovatively. As a recent
+                graduate, I am enthusiastic about contributing to front-end
+                development projects, where I can merge my artistic vision with
+                my technical skills. I am keen to join a dynamic team, apply my
+                creative and technical abilities, and continue growing in the
+                fast-paced field of software development.
+            </Text>
         </VStack>
     )
 }

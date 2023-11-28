3942f900267ef4f82cec6e5d82de1eefc4e41c59
Munkyfoot <wopas182@gmail.com>
2023-11-27 19:50:29 -0800

    Make portfolio page sub components direct children of Layout.

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -1,18 +1,16 @@
 import Layout from "@/components/Layout"
 import Academic from "@/components/portfolio/Academic"
 import Professional from "@/components/portfolio/Professional"
-import { VStack } from "@chakra-ui/react"
 
 export default function Home() {
     return (
         <Layout
             title="Portfolio"
             description="View Michael's portfolio of projects, spanning professional and academic work."
+            mainGap={8}
         >
-            <VStack align="flex-start" spacing={8}>
-                <Professional />
-                <Academic />
-            </VStack>
+            <Professional />
+            <Academic />
         </Layout>
     )
 }
